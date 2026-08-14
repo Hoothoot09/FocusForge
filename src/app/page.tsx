5c724cfed38039fa0f77e64df951884b9483fe0c
Andrei Soares <andreicarvalho1370@gmail.com>
2026-08-14 18:41:17 -0300

    Create Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-orange-500 text-4xl">ForcusForge</h1>
-      <p>Forge Your Progress</p>
-    </main>
-  );
+  return <main></main>;
 }
